df77e6f87a5b0951deb73694cc4abd68950c7b44
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2022-04-13 12:49:42 +1200

    Add files via upload

--- 04_bit_calc/03_integer_checker.py
+++ 04_bit_calc/03_integer_checker.py
@@ -1,4 +1,4 @@
-# checks number is more than zero
+#checks input is a number more than a given value
 def num_check(question, low):
     valid = False
     while not valid:
@@ -23,3 +23,17 @@ def num_check(question, low):
         except ValueError: 
             print(error)
 
+# Main Routine goes here  
+
+
+keep_going = ""
+while keep_going == "":
+    print()
+    # ask for integer (must be >= to zero)
+    var_int = num_check("Please enter an integer: ", 0)
+    print()
+
+    # ask for the width and heighth 0of an image (must be more)
+    image_width = num_check("Image width? ", 1)
+    print()
+    image_height = num_check("Image height? ", 1)
